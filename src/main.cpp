71f196de1852123e4bd22a2de7dd2833e1070441
Kai E. Frøland <kfrol@pm.me>
2026-04-08 20:51:55 +0200

    New functionality; Mantra encoder and decoder - which is compatible with any spawn point count! 🥳🥳🥳

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+#include <cstring>
 #include <iostream>
 #include "./fi/cli/Cli.h"
-
+#include "./mantra/MantraCli.h"
 
 int main(int argc, char** argv) try {
-	fi::Cli cli(argc, argv);
+
+	if (argc > 2 && std::strcmp(argv[1], "m") == 0) {
+		fman::MantraCli l_cli(argc, argv);
+		fman::Mantra l_man{ l_cli.get_mantra() };
+		std::cout << (l_cli.is_terse() ? l_man.get_mantra() : l_man.get_printable_summary());
+	}
+	else
+		fi::Cli cli(argc, argv);
 }
 catch (const std::runtime_error& ex) {
 	std::cerr << "Runtime error: " << ex.what() << "\n";
